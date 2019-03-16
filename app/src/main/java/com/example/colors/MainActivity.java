8a71cd8b29841d79b665003ec58a744fe8798544
estherlaraaaa <37164243+estherlaraaaa@users.noreply.github.com>
2019-03-15 21:46:20 -0600

    usando el onClick

--- app/src/main/java/com/example/colors/MainActivity.java
+++ app/src/main/java/com/example/colors/MainActivity.java
@@ -2,119 +2,51 @@ package com.example.colors;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
-import android.widget.Button;
 import android.widget.LinearLayout;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     LinearLayout btn_1;
     LinearLayout btn_2;
     LinearLayout btn_3;
-
-    int cont_btn_1=1;
-    int cont_btn_2=1;
-    int cont_btn_3=1;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btn_1=(LinearLayout)findViewById(R.id.btn_1);
-        btn_2=(LinearLayout)findViewById(R.id.btn_2);
-        btn_3=(LinearLayout)findViewById(R.id.btn_3);
+        btn_1 = findViewById(R.id.btn_1);
+        btn_2 = findViewById(R.id.btn_2);
+        btn_3 = findViewById(R.id.btn_3);
 
-        btn_1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if(cont_btn_1==0){
-                    btn_1.setBackgroundColor(getResources().getColor(R.color.first_btn));
-                    cont_btn_1=1;
-                }
-                else if(cont_btn_1==1){
-                    btn_1.setBackgroundColor(getResources().getColor(R.color.first_btn2));
-                    cont_btn_1=2;
-                }
-                else if(cont_btn_1==2){
-                    btn_1.setBackgroundColor(getResources().getColor(R.color.first_btn3));
-                    cont_btn_1=3;
-                }
-                else if(cont_btn_1==3){
-                    btn_1.setBackgroundColor(getResources().getColor(R.color.first_btn4));
-                    cont_btn_1=4;
-                }
-                else if(cont_btn_1==4){
-                    btn_1.setBackgroundColor(getResources().getColor(R.color.first_btn5));
-                    cont_btn_1=5;
-                }
-                else if(cont_btn_1==5){
-                    btn_1.setBackgroundColor(getResources().getColor(R.color.first_btn6));
-                    cont_btn_1=0;
-                }
-            }
-        });
+        btn_1.setOnClickListener(this);
+        btn_2.setOnClickListener(this);
+        btn_3.setOnClickListener(this);
+    }
 
-        btn_2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if(cont_btn_2==0){
-                    btn_2.setBackgroundColor(getResources().getColor(R.color.second_btn));
-                    cont_btn_2=1;
-                }
-                else if(cont_btn_2==1){
-                    btn_2.setBackgroundColor(getResources().getColor(R.color.second_btn2));
-                    cont_btn_2=2;
-                }
-                else if(cont_btn_2==2){
-                    btn_2.setBackgroundColor(getResources().getColor(R.color.second_btn3));
-                    cont_btn_2=3;
-                }
-                else if(cont_btn_2==3){
-                    btn_2.setBackgroundColor(getResources().getColor(R.color.second_btn4));
-                    cont_btn_2=4;
-                }
-                else if(cont_btn_2==4){
-                    btn_2.setBackgroundColor(getResources().getColor(R.color.second_btn5));
-                    cont_btn_2=5;
-                }
-                else if(cont_btn_2==5){
-                    btn_2.setBackgroundColor(getResources().getColor(R.color.second_btn6));
-                    cont_btn_2=0;
-                }
-            }
-        });
+    @Override
+    public void onClick(View v) {
+        int[] red = {R.color.first_btn, R.color.first_btn2, R.color.first_btn3, R.color.first_btn4, R.color.first_btn5, R.color.first_btn6};
 
-        btn_3.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if(cont_btn_3==0){
-                    btn_3.setBackgroundColor(getResources().getColor(R.color.third_btn));
-                    cont_btn_3=1;
-                }
-                else if(cont_btn_3==1){
-                    btn_3.setBackgroundColor(getResources().getColor(R.color.third_btn2));
-                    cont_btn_3=2;
-                }
-                else if(cont_btn_3==2){
-                    btn_3.setBackgroundColor(getResources().getColor(R.color.third_btn3));
-                    cont_btn_3=3;
-                }
-                else if(cont_btn_3==3){
-                    btn_3.setBackgroundColor(getResources().getColor(R.color.third_btn4));
-                    cont_btn_3=4;
-                }
-                else if(cont_btn_3==4){
-                    btn_3.setBackgroundColor(getResources().getColor(R.color.third_btn5));
-                    cont_btn_3=5;
-                }
-                else if(cont_btn_3==5){
-                    btn_3.setBackgroundColor(getResources().getColor(R.color.third_btn6));
-                    cont_btn_3=0;
-                }
-            }
-        });
+        int[] green = {R.color.second_btn,R.color.second_btn2,R.color.second_btn3,R.color.second_btn4,R.color.second_btn5,R.color.second_btn6};
 
+        int[] blue = {R.color.third_btn,R.color.third_btn2,R.color.third_btn3,R.color.third_btn4,R.color.third_btn5,R.color.third_btn6};
+
+        int viewId = v.getId();
+
+        Log.i("id ",viewId+"");
+
+        switch (viewId){
+            case R.id.btn_1:
+                btn_1.setBackgroundResource(red[(int) Math.floor(Math.random()*6)]);
+                break;
+            case R.id.btn_2:
+                btn_2.setBackgroundResource(green[(int) Math.floor(Math.random()*6)]);
+                break;
+            case R.id.btn_3:
+                btn_3.setBackgroundResource(blue[(int) Math.floor(Math.random()*6)]);
+                break;
+        }
     }
 }
